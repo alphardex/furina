73fb44a76a47a564f5ccdb9d519a3a70124efe20
alphardex <2582347430@qq.com>
2023-10-17 10:22:35 +0800

    overload fixNormal function

--- lighting/fixNormal.glsl
+++ lighting/fixNormal.glsl
@@ -24,3 +24,17 @@ vec3 fixNormal(vec3 position,vec3 distortedPosition,vec3 normal,float offset){
     vec3 displacedNormal=normalize(cross(displacedTangent,displacedBitangent));
     return displacedNormal;
 }
+
+vec3 fixNormal(vec3 position,vec3 normal,float offset){
+    vec3 tangent=orthogonal(normal);
+    vec3 bitangent=normalize(cross(normal,tangent));
+    vec3 neighbour1=position+tangent*offset;
+    vec3 neighbour2=position+bitangent*offset;
+    vec3 displacedNeighbour1=DISTORT_FNC(neighbour1);
+    vec3 displacedNeighbour2=DISTORT_FNC(neighbour2);
+    vec3 distortedPosition=DISTORT_FNC(position);
+    vec3 displacedTangent=displacedNeighbour1-distortedPosition;
+    vec3 displacedBitangent=displacedNeighbour2-distortedPosition;
+    vec3 displacedNormal=normalize(cross(displacedTangent,displacedBitangent));
+    return displacedNormal;
+}
